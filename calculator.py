0bb793085460d33377b51df7319349f263d2da14
Wooyang Son <sonwy102@gmail.com>
2020-09-30 23:06:08 +0000

    Completed power REPL

--- calculator.py
+++ calculator.py
@@ -5,3 +5,18 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 
 # Replace this with your code
+
+#Repeat foreever with while loop
+while True:
+    #read input
+    user_input = input("Enter your equation: ")
+    #use split function
+    tokens = user_input.split(' ')
+
+    #if first token is 'q', then quit
+    if tokens[0] == 'q': 
+        break
+    else:
+        if tokens[0] == 'pow':
+            print (power(float(tokens[1]), float(tokens[2])))
+            #print (answer)
