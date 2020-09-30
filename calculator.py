a26c5824bb0d086a572c96679a01525610a4a726
Wooyang Son <sonwy102@gmail.com>
2020-09-30 23:16:53 +0000

    Completed calculator REPL

--- calculator.py
+++ calculator.py
@@ -22,3 +22,23 @@ while True:
 
         elif tokens[0] == '+':
             print(add(float(tokens[1]), float(tokens[2])))
+
+        elif tokens[0] == '-':
+            print(subtract(float(tokens[1]), float(tokens[2])))
+
+        elif tokens[0] == '*':
+            print(multiply(float(tokens[1]), float(tokens[2])))
+        
+        elif tokens[0] == '/':
+            print(divide(float(tokens[1]), float(tokens[2])))
+
+        elif tokens[0] == 'square':
+            print(square(float(tokens[1])))
+        
+        elif tokens[0] == 'cube':
+            print(cube(float(tokens[1])))
+        
+        elif tokens[0] == 'mod' or tokens[0] == '%':
+            print(mod(float(tokens[1]), float(tokens[2])))
+        else:
+            print('invalid input')
